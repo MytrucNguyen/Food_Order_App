47749f12890f722b2207fa75b6bbe1dd41e1a43b
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-02-06 20:28:23 -0700

    Created a Meal Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Fragment } from 'react';
 import './App.css';
+// Components
+import Meals from './components/Meals';
 // Layout 
 import Header from './Layout/Header';
 
@@ -7,6 +9,9 @@ function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </Fragment>
   );
 }
