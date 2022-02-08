48a9ff3ec2cf9ccb52f15287f12104d73a647ae0
Mytruc Nguyen <80004058+MytrucNguyen@users.noreply.github.com>
2022-02-07 17:48:03 -0700

    Made Cart Button Animated When Item Is Added

--- src/Layout/CartButton/index.jsx
+++ src/Layout/CartButton/index.jsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 // mui
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
@@ -7,13 +7,33 @@ import classes from './index.module.css';
 
 export default function CartButton (props) {
     const cartContext = useContext(CartContext);
+    const [buttonIsHighlighted, setButtonIsHighlighted] = useState(false);
+
+    const { items } = cartContext;
 
     const numberOfCartItems = cartContext.items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0);
 
+    const buttonClasses = `${classes.button} ${buttonIsHighlighted ? classes.bump : ''}`;
+
+    useEffect(() => {
+        if (items.length === 0) {
+          return;
+        }
+        setButtonIsHighlighted(true);
+    
+        const timer = setTimeout(() => {
+            setButtonIsHighlighted(false);
+        }, 300);
+    
+        return () => {
+          clearTimeout(timer);
+        };
+      }, [items]);
+
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={buttonClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <ShoppingCartIcon />
             </span>
